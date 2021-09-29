a8be38782a0f2bacb076bc58cb3e9a9d4c56223b
Mohammed <mohams3ios01@gmail.com>
2021-09-29 11:13:30 -0400

    Setup Xcodegen with iOS and iOS Test Targets

---
+++ app/NounsWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NounsWatch WatchKit Extension
+//
+//  Created by Mohammed Ibrahim on 2021-09-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/NounsWatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  NounsWatch WatchKit Extension
+//
+//  Created by Mohammed Ibrahim on 2021-09-29.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
