b40fdc42e7e2ee658b07ce97ecd9e76e7469824d
Mohammed <mohams3ios01@gmail.com>
2021-10-25 22:38:08 -0400

    Cleanup

--- app/Shared/Services/ServicesTests/ENS/ENSFetchDomainTests.swift
+++ app/Shared/Services/ServicesTests/ENS/ENSFetchDomainTests.swift
@@ -15,75 +15,10 @@ final class ENSFetchDomainTests: XCTestCase {
   fileprivate static let domain = "example.eth"
 
   func testENSFetchDomain() throws {
-    /*
-     enum MockDataGraphQLResponder: MockGraphQLResponder {
-     static func respond() throws -> Data {
-     Data()
-     }
-     }
-     
-     // given
-     let graphQLClient = MockGraphQLClient<MockDataGraphQLResponder>()
-     let ens = TheGraphENSProvider(graphQLClient: graphQLClient)
-     
-     var cancellables = Set<AnyCancellable>()
-     let ensFetchExpectation = expectation(description: #function)
-     
-     // when
-     ens.fetchDomain(token: Self.token)
-     .sink { completion in
-     switch completion {
-     case .finished:
-     print("Finished")
-     case let .failure(error):
-     XCTFail("💥 Something went wrong: \(error)")
-     }
-     } receiveValue: { domain in
-     XCTAssertTrue(Thread.isMainThread)
-     XCTAssertEqual(domain, Self.domain)
-     
-     ensFetchExpectation.fulfill()
-     }
-     .store(in: &cancellables)
-     
-     // then
-     wait(for: [ensFetchExpectation], timeout: 1.0)
-     */
     fatalError("Implementation for \(#function) missing")
   }
 
   func testENSFetchDomainFailure() {
-    /*
-     enum MockFailureGraphQLResponder: MockGraphQLResponder {
-     static func respond() throws -> Data {
-     throw QueryError.noData
-     }
-     }
-     
-     // given
-     let graphQLClient = MockGraphQLClient<MockFailureGraphQLResponder>()
-     let ens = TheGraphENSProvider(graphQLClient: graphQLClient)
-     
-     var cancellables = Set<AnyCancellable>()
-     let ensFetchExpectation = expectation(description: #function)
-     
-     // when
-     ens.fetchDomain(token: Self.token)
-     .sink { completion in
-     if case let .failure(error) = completion {
-     XCTAssertEqual(error, ENSError.noDomain)
-     
-     ensFetchExpectation.fulfill()
-     }
-     } receiveValue: { domain in
-     XCTFail("💥 result unexpected")
-     }
-     .store(in: &cancellables)
-     
-     // then
-     wait(for: [ensFetchExpectation], timeout: 1.0)
-     }
-     */
     fatalError("Implementation for \(#function) missing")
   }
 }
--- app/Shared/Services/ServicesTests/GraphQLClient/GraphQLClientSubsciptionTests.swift
+++ app/Shared/Services/ServicesTests/GraphQLClient/GraphQLClientSubsciptionTests.swift
@@ -13,148 +13,21 @@ final class GraphQLClientSubsciptionTests: XCTestCase {
   
   /// Tests a successful request and response against the GraphQL client
   func testGraphQLClientSubscriptionSucceed() throws {
-    /*
-    // given
-    let subscriptionResult = MockApolloClient.MockSubscriptionResult(
-      data: [
-        AuctionSubscription.Data(auctions: [.init(id: GraphQLID("1"), amount: "0", startTime: "100", endTime: "120", settled: false)]),
-        AuctionSubscription.Data(auctions: [.init(id: GraphQLID("1"), amount: "0", startTime: "100", endTime: "120", settled: false)])
-      ],
-      errors: nil
-    )
-    // TODO: - Replace response data with fixtures
-    let mockApolloClient = MockApolloClient()
-    mockApolloClient.set(subscriptionResult: subscriptionResult, error: nil)
-    let client = ApolloGraphQLClient(apolloClient: mockApolloClient)
-    let subscription = MockSubscription<AuctionSubscription, AuctionList>(subscription: AuctionSubscription())
-
-    var cancellables = Set<AnyCancellable>()
-    let expectation = expectation(description: #function)
-    expectation.expectedFulfillmentCount = 2
-
-    // when
-    client.subscription(subscription)
-      .sink { completion in
-        switch completion {
-        case .finished:
-          print("Finished")
-        case let .failure(error):
-          XCTFail("💥 Something went wrong: \(error)")
-        }
-      } receiveValue: { nouns in
-        XCTAssertTrue(Thread.isMainThread)
-
-        expectation.fulfill()
-      }
-      .store(in: &cancellables)
-
-    // then
-    wait(for: [expectation], timeout: 1.0)
-     */
     fatalError("Implementation for \(#function) missing")
   }
   
   /// Tests a bad server response
   func testGraphQLClientSubscriptionFailureWithBadServerError() throws {
-    /*
-    // given
-    let expectedError = URLError(.badServerResponse)
-    let subscription = MockSubscription<AuctionSubscription, AuctionList>(
-      subscription: AuctionSubscription()
-    )
-    let mockApolloClient = MockApolloClient()
-    mockApolloClient.set(subscriptionResult: nil, error: expectedError)
-    let client = ApolloGraphQLClient(apolloClient: mockApolloClient)
-
-    var cancellables = Set<AnyCancellable>()
-    let expectation = expectation(description: #function)
-
-    // when
-    client.subscription(subscription)
-      .sink { completion in
-        if case let .failure(error) = completion {
-          XCTAssertTrue(Thread.isMainThread)
-          XCTAssertEqual(error, QueryError.request(error: expectedError))
-
-          expectation.fulfill()
-        }
-      } receiveValue: { domain in
-        XCTFail("💥 result unexpected")
-      }
-      .store(in: &cancellables)
-
-    // then
-    wait(for: [expectation], timeout: 1.0)
-     */
     fatalError("Implementation for \(#function) missing")
   }
   
   /// Tests when errors are returned from Apollo-related tasks
   func testGraphQLClientSubscriptionFailureWithErrors() throws {
-    /*
-    // given
-    let errors = [
-      GraphQLError(["description": "test description for error one"]),
-      GraphQLError(["description": "test description for error two"]),
-    ]
-
-    let mockApolloClient = MockApolloClient()
-    mockApolloClient.set(subscriptionResult: .init(data: nil, errors: errors), error: nil)
-    let client = ApolloGraphQLClient(apolloClient: mockApolloClient)
-    let subscription = MockSubscription<AuctionSubscription, AuctionList>(subscription: AuctionSubscription())
-
-    var cancellables = Set<AnyCancellable>()
-    let expectation = expectation(description: #function)
-
-    // when
-    client.subscription(subscription)
-      .sink { completion in
-        if case let .failure(error) = completion {
-          XCTAssertTrue(Thread.isMainThread)
-          XCTAssertEqual(error, errors.queryError())
-
-          expectation.fulfill()
-        }
-      } receiveValue: { domain in
-        XCTFail("💥 result unexpected")
-      }
-      .store(in: &cancellables)
-
-    // then
-    wait(for: [expectation], timeout: 1.0)
-     */
     fatalError("Implementation for \(#function) missing")
   }
   
   /// Tests when there are no errors, but when data is nil
   func testGraphQLClientSubscriptionFailureWithNoDataError() throws {
-    /*
-    // given
-    let mockApolloClient = MockApolloClient()
-    mockApolloClient.set(subscriptionResult: .init(data: nil, errors: nil), error: nil)
-    let client = ApolloGraphQLClient(apolloClient: mockApolloClient)
-    let subscription = MockSubscription<AuctionSubscription, AuctionList>(subscription: AuctionSubscription())
-    
-    var cancellables = Set<AnyCancellable>()
-    let expectation = expectation(description: #function)
-    
-    // when
-    client.subscription(subscription)
-      .sink { completion in
-        if case let .failure(error) = completion {
-          XCTAssertTrue(Thread.isMainThread)
-          XCTAssertEqual(error, QueryError.noData)
-          
-          expectation.fulfill()
-        }
-      } receiveValue: { domain in
-        XCTFail("💥 result unexpected")
-      }
-      .store(in: &cancellables)
-    
-    // then
-    wait(for: [expectation], timeout: 1.0)
-     */
     fatalError("Implementation for \(#function) missing")
   }
   
