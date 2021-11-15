876e8e3a10440f49ccf98c31baab94ba4455431a
Tamim ziad <tamim.ziad@gmail.com>
2021-11-15 23:31:07 +0100

    Provide documentation

--- app/Shared/UIComponents/Sources/Labels/CompoundLabel.swift
+++ app/Shared/UIComponents/Sources/Labels/CompoundLabel.swift
@@ -13,6 +13,22 @@ public struct CompoundLabel<Title>: View where Title: View {
     private let title: Title
     private let caption: String
     
+    /// Creates a label with a icon image, title view, and a caption.
+    ///
+    /// ```
+    /// CompoundLabel(SafeLabel("98.00", icon: .eth),
+    ///              icon: .currentBid,
+    ///              caption: "Current bid")
+    ///
+    /// CompoundLabel(Text("98.00"),
+    ///              icon: .currentBid,
+    ///              caption: "Current bid")
+    /// ```
+    ///
+    /// - Parameters:
+    ///    - title: A title view.
+    ///    - icon: An instance of image resource.
+    ///    - caption: of the title provided.
     public init(_ title: Title, icon: Image, caption: String) {
         self.icon = icon
         self.title = title
@@ -20,6 +36,7 @@ public struct CompoundLabel<Title>: View where Title: View {
     }
     
     public var body: some View {
+        
         HStack {
             icon
             VStack(alignment: .leading, spacing: 0) {
@@ -30,5 +47,17 @@ public struct CompoundLabel<Title>: View where Title: View {
                     .font(.custom(.regular, relativeTo: .footnote).monospacedDigit())
             }
         }
+    }
+}
+
+struct fefe: PreviewProvider {
+    
+    static var previews: some View {
+        CompoundLabel(Text("98.00"),
+                      icon: .currentBid,
+                      caption: "Current bid")
+            .onAppear {
+                UIComponents.configure()
+            }
     }
 }
