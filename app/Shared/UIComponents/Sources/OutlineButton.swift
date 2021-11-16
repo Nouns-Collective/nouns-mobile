2b08c8be003ee325c4501fa7ddf5249be873e9bf
Mohammed <mohams3ios01@gmail.com>
2021-11-15 21:41:02 -0500

    Add optional icon to buttons and made images resizable

--- app/Shared/UIComponents/Sources/OutlineButton.swift
+++ app/Shared/UIComponents/Sources/OutlineButton.swift
@@ -55,9 +55,9 @@ public struct OutlineButton<Label: View>: View {
     private let action: () -> Void
     
     /// The fill mode for the buttons height and width
-    private let fill: Set<Fill>
+    private var fill = Set<Fill>()
     
-    /// Initializes a standard button with a custom view for the label and a designated action for when the button is tapped
+    /// Initializes an outline button with a custom view for the label and a designated action for when the button is tapped
     ///
     /// Using a custom label view.
     ///
@@ -89,84 +89,63 @@ public struct OutlineButton<Label: View>: View {
         self.fill = fill
     }
     
-    /// Initializes a standard button with an optional system icon and optional text to create a standard button, as well as a designated action
+    /// Initializes an outline button with an optional  icon, text, and optional accessory image to create a standard button, as well as a designated action
     ///
-    /// Using a standard button label.
-    ///
-    /// ```swift
-    /// OutlineButton(systemImage: "xmark",
-    ///              text: "Cancel",
-    ///              action: {})
-    /// ```
-    ///
-    /// Using a standard button label, with only an icon.
+    /// Using a standard button label, with only text
     ///
     /// ```swift
-    /// OutlineButton(systemImage: "xmark",
-    ///              action: {})
-    ///
+    /// OutlineButton(icon: {
+    ///   Image(systemName: "hand.thumbsup.fill")
+    /// }, text: "Get Started", accessoryImage: {
+    ///     Image(systemName: "arrow.right")
+    /// }, action: {}, fill: [.width])
     /// ```
     ///
     /// - Parameters:
-    ///   - systemImage: The name of a system image for the button's icon (optional)
+    ///   - icon: The image for the button's icon (optional)
     ///   - text: The text for the button (optional)
+    ///   - accessoryImage: The accessory image of the button
     ///   - action: The action function for when the button is tapped
     ///   - fill: A value to set the fill mode for the button's height and width
     public init(
-        systemImage: String,
-        text: String? = nil,
+        @ViewBuilder icon: () -> Image? = { nil },
+        text: String,
+        @ViewBuilder accessoryImage: () -> Image? = { nil },
         action: @escaping () -> Void,
         fill: Set<Fill> = []
     ) where Label == StandardButtonLabel {
         self.label = {
-            return StandardButtonLabel(image: Image(systemName: systemImage), text: text, fullWidth: fill.contains(.width))
+            return StandardButtonLabel(icon: icon(), accessoryImage: accessoryImage(), text: text, fullWidth: fill.contains(.width))
         }()
         
         self.action = action
         self.fill = fill
     }
     
-    /// Initializes a standard button with an optional system icon and optional text to create a standard button, as well as a designated action
+    /// Initializes an outline button with only an icon as it's label, as well as a designated action
     ///
     /// Using a standard button label, with only text
     ///
     /// ```swift
-    /// OutlineButton(text: "Cancel",
-    ///              action: {})
-    ///
-    /// HStack {
-    ///     OutlineButton(text: "Cancel",
-    ///                  action: {},
-    ///                  fill: [.width])
-    ///         .frame(maxHeight: .infinity)
-    ///
-    ///     OutlineButton(text: "Save",
-    ///                  action: {},
-    ///                 fill: [.width])
-    ///         .frame(maxWidth: .infinity)
-    ///
-    ///  }.frame(height: 50)
+    /// OutlineButton(icon: {
+    ///     Image(systemName: "hand.thumbsup.fill")
+    /// }, action: {})
     /// ```
     ///
-    ///
     /// - Parameters:
-    ///   - image: The image for the button's icon (optional)
-    ///   - text: The text for the button (optional)
+    ///   - icon: The image for the button's icon
     ///   - action: The action function for when the button is tapped
-    ///   - fill: A value to set the fill mode for the button's height and width
     public init(
-        image: Image? = nil,
-        text: String? = nil,
-        action: @escaping () -> Void,
-        fill: Set<Fill> = []
-    ) where Label == StandardButtonLabel {
+        @ViewBuilder icon: () -> Image,
+        action: @escaping () -> Void
+    ) where Label == IconButtonLabel {
         self.label = {
-            return StandardButtonLabel(image: image, text: text, fullWidth: fill.contains(.width))
+            return IconButtonLabel(icon: icon())
         }()
         
         self.action = action
-        self.fill = fill
     }
+    
     
     public var body: some View {
         Button {
@@ -180,33 +159,63 @@ public struct OutlineButton<Label: View>: View {
 
 // TODO: Removes the preview and place it in the documentation
 struct OutlineButton_Previews: PreviewProvider {
-    static var previews: some View {
-        VStack {
-            VStack {
-                OutlineButton(systemImage: "arrow.right", text: "Get Started", action: {}, fill: [.width])
-                
-                HStack {
-                    OutlineButton(systemImage: "hand.thumbsup.fill", text: "Get Started", action: {}, fill: [.width])
-                    OutlineButton(systemImage: "hand.thumbsdown", text: "Get Started", action: {}, fill: [.width])
-                }
-            }.padding()
-                .background(Color.componentSeriousMango)
-            
-            VStack {
-                OutlineButton(systemImage: "arrow.right", text: "Get Started", action: {}, fill: [.width])
-                
-                HStack {
-                    OutlineButton(systemImage: "hand.thumbsup.fill", text: "Get Started", action: {}, fill: [.width])
-                    OutlineButton(systemImage: "hand.thumbsdown", text: "Get Started", action: {}, fill: [.width])
-                }
-                
-                HStack {
-                    OutlineButton(systemImage: "hand.thumbsup.fill", action: {})
-                    OutlineButton(systemImage: "hand.thumbsdown", text: "Get Started", action: {}, fill: [.width])
-                }
-            }.padding()
-        }.onAppear {
+    struct PreviewView: View {
+        init() {
             UIComponents.configure()
         }
+        
+        var body: some View {
+            VStack {
+                VStack {
+                    OutlineButton(text: "Get Started", accessoryImage: {
+                        Image(systemName: "arrow.right")
+                    }, action: {}, fill: [.width])
+                    
+                    OutlineButton(icon: {
+                        Image(systemName: "hand.thumbsup.fill")
+                    }, text: "Get Started", accessoryImage: {
+                        Image(systemName: "arrow.right")
+                    }, action: {}, fill: [.width])
+
+                    HStack {
+                        OutlineButton(text: "Get Started", accessoryImage: {
+                            Image(systemName: "hand.thumbsup.fill")
+                        }, action: {}, fill: [.width])
+                        OutlineButton(text: "Get Started", accessoryImage: {
+                            Image(systemName: "hand.thumbsdown")
+                        }, action: {}, fill: [.width])
+                    }
+                }.padding()
+                    .background(Color.componentSeriousMango)
+                
+                VStack {
+                    OutlineButton(text: "Get Started", accessoryImage: {
+                        Image(systemName: "arrow.right")
+                    }, action: {}, fill: [.width])
+                    
+                    HStack {
+                        OutlineButton(text: "Get Started", accessoryImage: {
+                            Image(systemName: "hand.thumbsup.fill")
+                        }, action: {}, fill: [.width])
+                        OutlineButton(text: "Get Started", accessoryImage: {
+                            Image(systemName: "hand.thumbsdown")
+                        }, action: {}, fill: [.width])
+                    }
+                    
+                    HStack {
+                        OutlineButton(icon: {
+                            Image(systemName: "hand.thumbsup.fill")
+                        }, action: {})
+                        OutlineButton(text: "Get Started", accessoryImage: {
+                            Image(systemName: "hand.thumbsdown")
+                        }, action: {}, fill: [.width])
+                    }
+                }.padding()
+            }
+        }
+    }
+    
+    static var previews: some View {
+        PreviewView()
     }
 }
