975f01af09a82cd0b8332dfd10aa96f88e24f5d4
Tamim ziad <tamim.ziad@gmail.com>
2021-11-19 15:23:29 +0100

    TabItem instead image names

--- app/Shared/UIComponents/Sources/OutlineTabView.swift
+++ app/Shared/UIComponents/Sources/OutlineTabView.swift
@@ -13,10 +13,26 @@ struct OutlineTabItemSelection: EnvironmentKey {
 }
 
 extension EnvironmentValues {
-
     var outlineTabItemSelection: Int {
         get { self[OutlineTabItemSelection.self] }
         set { self[OutlineTabItemSelection.self] = newValue }
+    }
+}
+
+/// Access all the items set by the client through the view modifier.
+struct OutlineTabItems: PreferenceKey {
+    static var defaultValue: [OutlineTabItem] = []
+    static func reduce(value: inout [OutlineTabItem], nextValue: () -> [OutlineTabItem]) {
+        value += nextValue()
+    }
+}
+
+/// Accessing the newly selected tab triggered by child views.
+struct OutlineTabItemSelectionChange: PreferenceKey {
+    static var defaultValue: Int = 0
+
+    static func reduce(value: inout Int, nextValue: () -> Int) {
+        value = nextValue()
     }
 }
 
@@ -27,15 +43,9 @@ struct OutlineTabItem: Hashable, Identifiable {
     let tag: Int
 }
 
-struct OutlineTabItems: PreferenceKey {
-    static var defaultValue: [OutlineTabItem] = []
-    static func reduce(value: inout [OutlineTabItem], nextValue: () -> [OutlineTabItem]) {
-        value += nextValue()
-    }
-}
-
 internal struct OutlineTabBar: View {
     @Environment(\.outlineTabItemSelection) private var selection
+    @State private var isSelected = false
     private let items: [OutlineTabItem]
     
     internal init(_ items: [OutlineTabItem]) {
@@ -45,14 +55,35 @@ internal struct OutlineTabBar: View {
     internal var body: some View {
         HStack {
             ForEach(items) { item in
-                Image(item.icon, bundle: .module)
+                tabItem(item)
+                    .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
         }
         .padding(.vertical, 20)
         .padding(0)
         .border(.black, width: 2)
         .background(Color.white)
+    }
+    
+    private func tabItem(_ item: OutlineTabItem) -> some View {
+        Image(item.icon, bundle: .module)
+            .preference(
+                key: OutlineTabItemSelectionChange.self,
+                value: (isSelected ? item.tag : selection)
+            )
+            .onTapGesture {
+                withAnimation(.spring(blendDuration: 0.05)) {
+                    isSelected = true
+                }
+            }
+            .onChange(of: selection) { newValue in
+                if newValue != item.tag {
+                    isSelected = false
+                }
+            }
+            .onAppear {
+                isSelected = (selection == item.tag)
+            }
     }
 }
 
@@ -86,6 +117,9 @@ public struct OutlineTabView<Content>: View where Content: View {
         .onPreferenceChange(OutlineTabItems.self) { items in
             self.items = items
         }
+        .onPreferenceChange(OutlineTabItemSelectionChange.self) { newSelectedTabItem in
+            selection = newSelectedTabItem
+        }
     }
 }
 
@@ -97,14 +131,13 @@ struct OutlineTabView_preview: PreviewProvider {
                 .outlineTabItem("explore-outline", tag: 0)
             
             Gradient.bubbleGum
-                .outlineTabItem("create-outline", tag: 0)
+                .outlineTabItem("create-outline", tag: 1)
             
             Gradient.lemonDrop
-                .outlineTabItem("play-outline", tag: 0)
+                .outlineTabItem("play-outline", tag: 2)
             
             Gradient.orangesicle
-                .outlineTabItem("settings-outline", tag: 0)
+                .outlineTabItem("settings-outline", tag: 3)
         }
     }
 }
-
