571ebcd248ae152f78a251916f899d99c776633b
Tamim ziad <tamim.ziad@gmail.com>
2021-11-20 23:22:40 +0100

    Display DAO Governance Info

--- app/iOS/Sources/Components/Explore/NounderView.swift
+++ app/iOS/Sources/Components/Explore/NounderView.swift
@@ -12,7 +12,9 @@ import Services
 struct NounderView: View {
   @EnvironmentObject var store: AppStore
   @State private var selectedTab = 0
+  @State private var isInfoPresented = false
   @Binding var isPresented: Bool
+  @Environment(\.openURL) private var openURL
   
   let noun: Noun
   
@@ -36,8 +38,38 @@ struct NounderView: View {
     }, rightAccessory: {
       SoftButton(
         icon: { Image.help },
-        action: { })
+        action: { isInfoPresented.toggle() })
     })
     .background(Gradient.warmGreydient)
+    .bottomSheet(isPresented: $isInfoPresented) {
+      VStack(alignment: .leading, spacing: 20) {
+        HStack {
+          Text("What is this?")
+            .font(.custom(.bold, size: 36))
+          
+          Spacer()
+          
+          SoftButton(
+            icon: { Image.xmark },
+            action: { isInfoPresented.toggle() })
+        }
+        
+        Text("Each noun is a member of the Nouns DAO and entitled to one vote in all governance matters. This means, once Noun 62 was owned by beautifulpunks.eth, they could vote on proposals to the DAO and this is their voting activity.")
+          .font(.custom(.regular, size: 17))
+          .lineSpacing(5)
+        
+        SoftButton(
+          text: "Learn more",
+          smallAccessory: { Image.squareArrowDown },
+          action: {
+            if let url = URL(string: "https://nouns.wtf") {
+              openURL(url)
+            }
+          },
+          fill: [.width])
+      }
+      .padding(16)
+      .padding(.bottom, 4)
+    }
   }
 }
