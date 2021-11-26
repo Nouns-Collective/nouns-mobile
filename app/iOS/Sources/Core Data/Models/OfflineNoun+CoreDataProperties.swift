2e2bfedd5876305bcfc19f17ceaab8780a9a7c2d
Mohammed Ibrahim <mothecreator@icloud.com>
2021-11-26 15:44:25 -0500

    Add Offline Noun Card

--- app/iOS/Sources/Core Data/Models/OfflineNoun+CoreDataProperties.swift
+++ app/iOS/Sources/Core Data/Models/OfflineNoun+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  OfflineNoun+CoreDataProperties.swift
 //  Nouns
 //
-//  Created by Mohammed Ibrahim on 2021-11-23.
+//  Created by Mohammed Ibrahim on 2021-11-26.
 //
 //
 
@@ -16,29 +16,17 @@ extension OfflineNoun {
     return NSFetchRequest<OfflineNoun>(entityName: "OfflineNoun")
   }
   
-  /// The unique ID of the Noun
-  @NSManaged public var id: UUID
-  
-  /// The date the offline noun was created
-  @NSManaged public var createdDate: Date
-  
-  /// The user-defined name of the noun
+  @NSManaged public var accessory: String?
+  @NSManaged public var background: [String]?
+  @NSManaged public var body: String?
+  @NSManaged public var createdDate: Date?
+  @NSManaged public var glasses: String?
+  @NSManaged public var head: String?
+  @NSManaged public var id: UUID?
   @NSManaged public var name: String?
   
-  /// The background asset image name
-  @NSManaged public var background: String?
-  
-  /// The glasses asset image name
-  @NSManaged public var glasses: String?
-  
-  /// The head asset image name
-  @NSManaged public var head: String?
-  
-  /// The body asset image name
-  @NSManaged public var body: String?
-  
-  /// The accessory asset image name
-  @NSManaged public var accessory: String?
 }
 
-extension OfflineNoun: Identifiable {}
+extension OfflineNoun: Identifiable {
+  
+}
