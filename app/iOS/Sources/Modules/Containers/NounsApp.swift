34449e8768a18df1adda8b4593ac87e61cbd5de4
Ziad Tamim <hey@byziad.com>
2021-12-21 21:12:15 +0100

    Remove useless code

--- app/iOS/Sources/Modules/Containers/NounsApp.swift
+++ app/iOS/Sources/Modules/Containers/NounsApp.swift
@@ -51,20 +51,14 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
     Messaging.messaging().delegate = self
     
-    if #available(iOS 10.0, *) {
-      // For iOS 10 display notification (sent via APNS)
-      UNUserNotificationCenter.current().delegate = self
+    // For iOS 10 display notification (sent via APNS)
+    UNUserNotificationCenter.current().delegate = self
 
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-    } else {
-      let settings: UIUserNotificationSettings =
-        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-      application.registerUserNotificationSettings(settings)
-    }
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in }
+    )
 
     application.registerForRemoteNotifications()
 
@@ -96,7 +90,6 @@ extension AppDelegate: MessagingDelegate {
       userInfo: dataDict
     )
   }
-
 }
 
 /// User Notifications
