a1400798d82cf4c994287e0a3fe2d012542c75a0
Ziad Tamim <hey@byziad.com>
2022-01-03 21:00:52 +0100

    Firebase Crashlytics integration

--- app/Shared/Services/Package.swift
+++ app/Shared/Services/Package.swift
@@ -28,6 +28,7 @@ let package = Package(
       dependencies: [
         "web3.swift",
         .product(name: "FirebaseAnalytics", package: "Firebase"),
+        .product(name: "FirebaseCrashlytics", package: "Firebase"),
       ],
       path: "Sources",
       resources: [.process("Resources")]
---
+++ app/Shared/Services/Sources/CrashReporting/CrashReporting.swift
@@ -0,0 +1,32 @@
+//
+//  CrashReporting.swift
+//  
+//
+//  Created by Ziad Tamim on 03.01.22.
+//
+
+import Firebase
+import FirebaseCrashlytics
+
+public protocol CrashReporting {
+  
+  /// Records a non-fatal event described by an NSError object. The events are
+  /// grouped and displayed similarly to crashes.
+  ///
+  ///  - Parameters:
+  ///   - error: Non-fatal error to be recorded
+  func record(error: Error)
+}
+
+public class CrashlyticsProvider: CrashReporting {
+ 
+  public init() {
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
+  }
+  
+  public func record(error: Error) {
+    Crashlytics.crashlytics().record(error: error)
+  }
+}
