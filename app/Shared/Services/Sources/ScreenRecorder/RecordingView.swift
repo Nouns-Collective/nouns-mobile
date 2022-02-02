32390739adc2f49ce8506c3db3df5123a648c2ae
Mohammed Ibrahim <mothecreator@icloud.com>
2022-02-01 20:26:52 -0500

    Address comments

--- app/Shared/Services/Sources/ScreenRecorder/RecordingView.swift
+++ app/Shared/Services/Sources/ScreenRecorder/RecordingView.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-/// A view used to construct a custom view to record, with a custom main view and background view
+/// A wrapper for the view to record using the `ScreenRecorder`
+///
+/// The `RecordingView` can hold an instance of a main view (`sourceView`) and an optional
+/// background view (`backgroundView`). The two views are layered together and presented as one view
+/// to record frame-by-frame. Each frame is flattened into an image with the background in the
+/// back of the frame and the source view on top.
 internal class RecordingView: UIView {
   
   private var sourceView: UIView
